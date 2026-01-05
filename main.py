3215ed83fd20a39e9e1507a706a41c787b261683
vansh chauhan <vanshchauhan2021@outlook.com>
2026-01-05 23:48:26 +0530

    enhance heatmap visualization

--- main.py
+++ main.py
@@ -19,9 +19,13 @@ hidden_dim = st.slider(
     "Select hidden dimension size:", min_value=5, max_value=50, value=10, step=5
 )
 
-# selection for random seed 
+# selection for random seed
 random_seed = st.number_input(
-    "Set random seed for reproducibility:", min_value=0, max_value=1000, value=42, step=1
+    "Set random seed for reproducibility:",
+    min_value=0,
+    max_value=1000,
+    value=42,
+    step=1,
 )
 
 # button to trigger sentence processing
@@ -35,7 +39,7 @@ if st.button("Process Sentence"):
         set_random_seed(random_seed)
         tokenizer = SampleTokenizer(user_sentence, hidden_dim)
         attention_layer = Attention(dim_in=hidden_dim, dim_out=hidden_dim)
-        output = tokenizer.embedded.unsqueeze(0)  
+        output = tokenizer.embedded.unsqueeze(0)
         attention_score = attention_layer.forward(output)
 
         st.subheader("Results")
@@ -49,25 +53,26 @@ if st.button("Process Sentence"):
 
         # displaying the attention scores as a heatmap
         st.write("Self-Attention Scores Heatmap:")
-        fig, ax = plt.subplots()    
-        cax = ax.matshow(attention_score[0].detach().numpy(), cmap="viridis") 
-        fig.colorbar(cax) 
+        fig, ax = plt.subplots()
+        fig.patch.set_alpha(0)  # Transparent figure background
+        ax.patch.set_alpha(0)  # Transparent axes background
+        cax = ax.matshow(attention_score[0].detach().numpy(), cmap="YlOrRd")
+        cbar = fig.colorbar(cax)
+        cbar.ax.yaxis.set_tick_params(color="white")
+        # cbar.outline.set_edgecolor("white")
+        plt.setp(plt.getp(cbar.ax.axes, "yticklabels"), color="white")
         ax.set_xticks(range(len(tokenizer.encoded)))
         ax.set_yticks(range(len(tokenizer.encoded)))
-        ax.set_xticklabels(user_sentence.split(' '), rotation=90)
-        ax.set_yticklabels(user_sentence.split(' '))
-        st.pyplot(fig)
+        ax.set_xticklabels(user_sentence.split(" "), rotation=90, color="white")
+        ax.set_yticklabels(user_sentence.split(" "), color="white")
+        ax.tick_params(axis="both", colors="white")  # Change tick marks color to white
+        st.pyplot(fig, transparent=True)
         st.write("Output Tensor Shape:", output.shape)
 
         # displaying the queries, keys, and values tensors
         st.write("Queries Tensor:")
-        st.table(pd.DataFrame(attention_layer.queries[0].detach().numpy())) 
+        st.table(pd.DataFrame(attention_layer.queries[0].detach().numpy()))
         st.write("Keys Tensor:")
         st.table(pd.DataFrame(attention_layer.keys[0].detach().numpy()))
         st.write("Values Tensor:")
         st.table(pd.DataFrame(attention_layer.values[0].detach().numpy()))
-
-
-
-
-    
